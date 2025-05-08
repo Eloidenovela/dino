0a298adb93350354755744b5c7d7a852983801ec
Eloide Novela <eloide.novela@outlook.com>
2025-05-08 21:33:58 +0200

    feat(algorithm): added namespace math

--- src/algorithm/util/util.cpp
+++ src/algorithm/util/util.cpp
@@ -8,4 +8,19 @@ namespace util
         p1 = p2;
         p2 = aux;
     }
+
+namespace math 
+{
+    bool is_prime(int number)
+    {
+        if (number <= 1) return false;
+
+        for (int i = 2; i <= (number/2); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
 }
--- src/algorithm/util/util.hpp
+++ src/algorithm/util/util.hpp
@@ -3,4 +3,9 @@
 namespace util 
 {
     void swap(int & p1, int & p2);
+    
+namespace math 
+{
+    bool is_prime(int number);
+}
 }
